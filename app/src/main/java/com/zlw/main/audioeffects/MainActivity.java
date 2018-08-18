5996b1ad37fe654cfa4f8f7a0fa044154f97441e
zhaolewei <739043667@qq.com>
2018-08-18 22:16:56 +0800

    使用傅里叶频谱

--- app/src/main/java/com/zlw/main/audioeffects/MainActivity.java
+++ app/src/main/java/com/zlw/main/audioeffects/MainActivity.java
@@ -5,12 +5,18 @@ import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import com.zlw.main.audioeffects.player.AudioVisualConverter;
 import com.zlw.main.audioeffects.player.MyMediaPlayer;
 import com.zlw.main.audioeffects.utils.Logger;
 import com.zlw.main.audioeffects.view.AudioView;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,19 +25,33 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.MODIFY_AUDIO_SETTINGS
     };
+
+    private static final Map<String, Integer> MUSIC_DATA = new HashMap<>();
+
+    {
+        MUSIC_DATA.put("剑墨.mp3", R.raw.mo);
+        MUSIC_DATA.put("grace.mp3", R.raw.grace);
+        MUSIC_DATA.put("faded.mp3", R.raw.faded);
+    }
+
     private Visualizer visualizer;
+    private AudioVisualConverter audioVisualConverter;
     private boolean isInit = false;
-    private AudioView audioView;
+
+    private AudioView audioView, audioView2;
+    private Spinner spinner;
 
 
     private Visualizer.OnDataCaptureListener dataCaptureListener = new Visualizer.OnDataCaptureListener() {
         @Override
         public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
-            audioView.setWaveData(waveform);
+            audioView.setWaveData(audioVisualConverter.readyDataByte(waveform));
         }
 
         @Override
         public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+//            Logger.d(TAG, "");
+            audioView2.setWaveData(fft);
         }
     };
 
@@ -39,15 +59,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        audioView = findViewById(R.id.audioView);
         ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, 1);
-        play();
+        initView();
+        initEvent();
+        play(R.raw.grace);
     }
 
-    private void play() {
+    private void initEvent() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                MUSIC_DATA.keySet().toArray(new String[MUSIC_DATA.size()]));
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object item = parent.getItemAtPosition(position);
+                Logger.i(TAG, "选择： %s", item.toString());
+                play(MUSIC_DATA.get(item.toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_main);
+        audioView = findViewById(R.id.audioView);
+        audioView2 = findViewById(R.id.audioView2);
+        spinner = findViewById(R.id.spinner);
+    }
+
+    private void play(int res) {
+        isInit = false;
         MyMediaPlayer mediaPlayer = MyMediaPlayer.getInstance();
-        mediaPlayer.play(R.raw.grace);
+        mediaPlayer.play(res);
         mediaPlayer.setPlayStateListener(new MyMediaPlayer.PlayStateListener() {
             @Override
             public void onStateChange(MyMediaPlayer.PlayState state) {
@@ -63,14 +110,18 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         isInit = true;
-
+        audioVisualConverter = new AudioVisualConverter();
         Logger.d(TAG, "initVisualizer()");
         try {
             int mediaPlayerId = MyMediaPlayer.getInstance().getMediaPlayerId();
             Logger.i(TAG, "mediaPlayerId: %s", mediaPlayerId);
+            if (visualizer != null) {
+                visualizer.release();
+            }
             visualizer = new Visualizer(mediaPlayerId);
+
             visualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[0]);
-            visualizer.setDataCaptureListener(dataCaptureListener, Visualizer.getMaxCaptureRate() / 2, true, false);
+            visualizer.setDataCaptureListener(dataCaptureListener, Visualizer.getMaxCaptureRate() / 2, true, true);
             visualizer.setEnabled(true);
         } catch (Exception e) {
             Logger.e(TAG, "请检查录音权限");
--- app/src/main/java/com/zlw/main/audioeffects/view/AudioView.java
+++ app/src/main/java/com/zlw/main/audioeffects/view/AudioView.java
@@ -8,15 +8,6 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.zlw.main.audioeffects.utils.Logger;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static android.content.ContentValues.TAG;
-
 /**
  * @author zhaolewei on 2018/8/17.
  */
@@ -28,9 +19,11 @@ public class AudioView extends View {
     private static final int LUMP_COUNT = 128;
     private static final int LUMP_WIDTH = 6;
     private static final int LUMP_MIN_HEIGHT = LUMP_WIDTH;
-    private static final int LUMP_MAX_HEIGHT = 200;
+    private static final int LUMP_MAX_HEIGHT = 200;//TODO: HEIGHT
     private static final int LUMP_SPACE = 2;
     private static final int LUMP_COLOR = Color.parseColor("#FFBBFF");
+
+    private static final float scale = 2;
 
 
     private byte[] waveData;
@@ -58,73 +51,8 @@ public class AudioView extends View {
         lumpPaint.setColor(LUMP_COLOR);
     }
 
-    Map<Integer, Byte> maxValueMap;
-
     public void setWaveData(byte[] data) {
-        maxValueMap = new HashMap<>();
-        List<Byte> list = new ArrayList<>();
-        for (int i = 0; i < data.length; i++) {
-            list.add((byte) (128 - Math.abs(data[i])));
-        }
-        byte max = 0;
-        int maxIndex = 0;
-        byte last = 0;
-
-        for (int i = 0; i < 32; i++) {
-            Byte value = list.get(i);
-            if (value > max) {
-                max = value;
-                maxIndex = i;
-            }
-        }
-        byte value1 = (byte) (max * 1.5);
-        if (value1 < 0) {
-            value1 = 127;
-        }
-
-        byte maxValue = value1;
-        byte maxValue11 = (byte) (maxValue * Math.random());
-        byte maxValue12 = (byte) (maxValue * Math.random());
-        byte maxValue21 = (byte) (maxValue11 * Math.random());
-        byte maxValue22 = (byte) (maxValue12 * Math.random());
-
-        if (maxIndex - 2 >= 0) {
-            maxValueMap.put(maxIndex - 2, maxValue21);
-        }
-        if (maxIndex - 1 >= 0) {
-            maxValueMap.put(maxIndex - 1, maxValue11);
-        }
-        maxValueMap.put(maxIndex, maxValue);
-        maxValueMap.put(maxIndex + 1, maxValue12);
-        maxValueMap.put(maxIndex + 2, maxValue22);
-
-
-        max = 0;
-        int laseIndex = 0;
-        last = 0;
-        for (int i = 32; i < list.size(); i++) {
-            Byte value = list.get(i);
-            if (value > max) {
-                max = value;
-                maxIndex = i;
-            } else {
-                //开始记录制高点
-                if (value < last) {
-                    if (maxIndex - laseIndex > 8) {
-                        maxValueMap.put(maxIndex, (byte) (maxValue/6));
-                        laseIndex = maxIndex;
-                    }
-                    max = 0;
-                }
-            }
-            last = value;
-        }
-
-        waveData = new byte[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            waveData[i] = list.get(i);
-        }
-
+        this.waveData = data;
         postInvalidate();
     }
 
@@ -134,18 +62,20 @@ public class AudioView extends View {
         for (int i = 0; i < LUMP_COUNT; i++) {
 
             if (waveData == null) {
-                canvas.drawRect((LUMP_WIDTH + LUMP_SPACE) * i, LUMP_MAX_HEIGHT - LUMP_MIN_HEIGHT, (LUMP_WIDTH + LUMP_SPACE) * i + LUMP_WIDTH, LUMP_MAX_HEIGHT, lumpPaint);
+                canvas.drawRect((LUMP_WIDTH + LUMP_SPACE) * i,
+                        LUMP_MAX_HEIGHT - LUMP_MIN_HEIGHT,
+                        (LUMP_WIDTH + LUMP_SPACE) * i + LUMP_WIDTH,
+                        LUMP_MAX_HEIGHT,
+                        lumpPaint);
                 continue;
             }
 
-            Byte value = maxValueMap.get(i);
-            if (value == null) {
-                canvas.drawRect((LUMP_WIDTH + LUMP_SPACE) * i, LUMP_MAX_HEIGHT - LUMP_MIN_HEIGHT, (LUMP_WIDTH + LUMP_SPACE) * i + LUMP_WIDTH, LUMP_MAX_HEIGHT, lumpPaint);
-                continue;
-            }
-
-
-            canvas.drawRect((LUMP_WIDTH + LUMP_SPACE) * i, (float) (LUMP_MAX_HEIGHT - LUMP_MIN_HEIGHT - value * 1.5), (LUMP_WIDTH + LUMP_SPACE) * i + LUMP_WIDTH, LUMP_MAX_HEIGHT, lumpPaint);
+            int value = waveData[i];
+            canvas.drawRect((LUMP_WIDTH + LUMP_SPACE) * i,
+                    (LUMP_MAX_HEIGHT - LUMP_MIN_HEIGHT - value * scale),
+                    (LUMP_WIDTH + LUMP_SPACE) * i + LUMP_WIDTH,
+                    LUMP_MAX_HEIGHT,
+                    lumpPaint);
 
         }
     }
